39283e11e8c4f445b5b9af539b08eed6688c616e
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-04 12:02:48 +0530

    Create Day3.1.py

---
+++ Day3.1.py
@@ -0,0 +1,8 @@
+## if-else with comparision operator
+print("welcome to the rollcoaster")
+height=int(input("what is your height in cm?"))
+
+if height>=120:
+    print("you can ride the rollcoaster")
+else:
+    print("sorry,you can't ride the rollcoaster")
