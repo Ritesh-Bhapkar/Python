522c917d0595a2b4c503250114ae37dc31eca3af
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-04 12:25:40 +0530

    Create Day-3.2.py

---
+++ Day-3.2.py
@@ -0,0 +1,7 @@
+## Even or odd With Comparision opeartor ##
+
+number=int(input("which number do you want to check: "))
+if number%2==0:
+    print("This is Even number")
+else:
+    print("This is odd number")
