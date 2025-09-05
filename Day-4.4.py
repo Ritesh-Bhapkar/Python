01c2721ae57938f55dbc13fe1f08262a21321544
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-05 18:58:22 +0530

    Create Day-4.4.py

---
+++ Day-4.4.py
@@ -0,0 +1,13 @@
+veggies=["Strawberries",
+    "Nectarines",
+    "Spinach",
+    "Strawberries",
+    "Kale/Collard/Mustard Greens",
+    "Grapes",
+    "Peaches",
+    "Cherries"
+    ]
+veggies[-1]="Melons"
+veggies.append("Lemons")
+print(veggies)
+
