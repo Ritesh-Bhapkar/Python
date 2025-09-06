71d33091d4d2185240659f3b1ee8a3ffc32bf01e
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-06 17:20:47 +0530

    Create Day-5.4.py

---
+++ Day-5.4.py
@@ -0,0 +1,11 @@
+## for loop using range function ##
+
+for i in range(1,11,3):
+    print(i)
+
+
+## using range calculated sum of all number ##
+sum=0
+for j in range(1,101):
+    sum+=j
+print(sum)
