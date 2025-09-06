eddf6f4e77bd485c404bc8ecd31c31a2749fbaf5
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-06 16:31:13 +0530

    Create Day-5,1.py

---
+++ Day-5,1.py
@@ -0,0 +1,7 @@
+fruits=["Apples", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit+" Pie")
+  ##print(fruits)  it will print exexcuted after every loop execution bcz of indentation
+print(fruits)    ## this will only one will be printed bcz outside of for loop
