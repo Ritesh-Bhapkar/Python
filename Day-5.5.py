0e9afe629e311310a2f087bf80f472ca2e28f357
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-06 17:59:51 +0530

    Create Day-5.5.py

---
+++ Day-5.5.py
@@ -0,0 +1,14 @@
+## first way !! ##
+
+sum1=0
+for i in range(2,101,2):
+    sum1+=i
+print(sum1)
+
+
+ ## Sencond way ##   
+sum=0
+for i in range(1,101):
+    if i%2==0:
+        sum+=i
+print(sum)
