98ca5e0d62d9db6c4a2ab3c2ffac22dc0616af4e
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-08 19:58:19 +0530

    Create Day-6-Hurdel-4-Game.py

---
+++ Day-6-Hurdel-4-Game.py
@@ -0,0 +1,24 @@
+## with some more condition ##
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def turn():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        turn()
+    else:
+        move()
