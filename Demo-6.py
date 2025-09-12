167313fa7e79689b08b92c28ad9cce4faaf94b8c
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-12 23:42:34 +0530

    Delete Demo-6.py

--- Demo-6.py
+++
@@ -1,3 +0,0 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")         #here "print("what to print")"  1) string --"print(" 2) code -- what to print 3)  string --")"
