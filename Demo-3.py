bfe3754b1875efa33d520290044687a27017627f
Ritesh Bhapkar <bhapkarritesh@gmail.com>
2025-09-25 22:06:48 +0530

    Create Demo-3.py

---
+++ Demo-3.py
@@ -0,0 +1,3 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")         #here "print("what to print")"  1) string --"print(" 2) code -- what to print 3)  string --")"
